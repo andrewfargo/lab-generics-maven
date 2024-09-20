39c5670ca8de16f8234db8d33b822cfa7d9a6440
Drew Fargo <drew.fargo@gmail.com>
2024-09-20 18:40:47 -0500

    Complete work from Exercise 1

--- src/main/java/edu/grinnell/csc207/experiments/SimpleDynamicArrayExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/SimpleDynamicArrayExperiments.java
@@ -56,5 +56,40 @@ public class SimpleDynamicArrayExperiments {
     for (int i = 0; i < 10; i++) {
       pen.println(numbers.get(i) + "^2 = " + square(numbers.get(i)));
     } // for i
+
+    // Exercise 1
+
+    DynamicArray<String> strings = new SimpleDynamicArray<String>();
+  
+    for (int i = 0; i < 10; i++) {
+      strings.set(i, "a".repeat(i));
+    } // for
+
+    for (int i = 0; i < 10; i++) {
+      pen.println("strings[" + i + "] = " + strings.get(i));
+    } // for
+
+    // strings.set(10, BigInteger.ZERO);
+    // The previous line complains that set(int, BigInteger) is not defined for
+    // DynamicArray<String>
+
+    // The following works but the compiler states you should parameterize
+    // the raw type, as well as needing an unchecked conversion.
+    /*
+    DynamicArray<BigInteger> numbers2 = new SimpleDynamicArray();
+    numbers2.set(0, BigInteger.ZERO);
+    pen.println(numbers2.get(0));
+    */
+
+    // Almost shockingly, these both work due to an implicit
+    // cast to Object, but any chance at undefined interaction will break.
+    /*
+    DynamicArray numbers3 = new SimpleDynamicArray();
+    numbers3.set(0, BigInteger.ZERO);
+    pen.println(numbers3.get(0));
+    numbers3.set(1, "Hello");
+    pen.println(numbers3.get(1));
+    numbers3.set(2, numbers3.get(0) + numbers3.get(1)); // doesn't compile
+    */
   } // main(String[])
 } // class SimpleDynamicArrayExperiment
